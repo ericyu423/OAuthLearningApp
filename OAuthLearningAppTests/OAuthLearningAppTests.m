a15224c2d972017449d56f418bbf2e91a8f9d4f2
ericyu423 <ericyu423@hotmail.com>
2018-05-25 16:20:16 -0700

    Initial Commit

---
+++ OAuthLearningAppTests/OAuthLearningAppTests.m
@@ -0,0 +1,39 @@
+//
+//  OAuthLearningAppTests.m
+//  OAuthLearningAppTests
+//
+//  Created by eric yu on 5/25/18.
+//  Copyright © 2018 eric yu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OAuthLearningAppTests : XCTestCase
+
+@end
+
+@implementation OAuthLearningAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
